fcbe4e8b539e8204a3eec3262caa85d26cf9c5b1
s2000promax <s2000promax@gmail.com>
2023-08-27 00:23:48 +0400

    feat: add rating feature, entity

---
+++ client/src/entities/Rating/index.ts
@@ -0,0 +1,3 @@
+export type { RatingI } from './model/types/rating.interface';
+
+export { useGetRating, useRate } from './model/api/rating.api';
---
+++ client/src/entities/Rating/model/types/rating.interface.ts
@@ -0,0 +1,4 @@
+export interface RatingI {
+    rate: number;
+    feedback?: string;
+}
