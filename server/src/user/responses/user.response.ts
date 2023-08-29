e2a9d71e6eb96a954321818be7cbcf3e522472d4
s2000promax <s2000promax@gmail.com>
2023-08-30 03:58:35 +0400

    feat: add user module

---
+++ server/src/user/responses/user.response.ts
@@ -0,0 +1,19 @@
+import { User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+
+export class UserResponse implements User {
+    id: string;
+    email: string;
+    createdAt: Date;
+    updatedAt: Date;
+    settings: number;
+    isBlocked: boolean;
+    roles: string;
+
+    @Exclude()
+    password: string;
+
+    constructor(user: User) {
+        Object.assign(this, user);
+    }
+}
