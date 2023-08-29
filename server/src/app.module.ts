87882153ce6e24b63b9243fb7c9523ef9a6126ba
s2000promax <s2000promax@gmail.com>
2023-08-30 02:42:12 +0400

    feat: add app config, swagger

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfiguration from './config/app/appConfig';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [
+        ConfigModule.forRoot({
+            load: [appConfiguration],
+            isGlobal: true,
+        }),
+        PrismaModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
