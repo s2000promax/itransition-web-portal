b6c41aad23c3b485c8ba9c6a5d66721ef102a35a
s2000promax <s2000promax@gmail.com>
2023-09-25 14:05:44 +0400

    fix: route from recommendation

--- client/src/features/Review/ui/Review.tsx
+++ client/src/features/Review/ui/Review.tsx
@@ -1,11 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useEffect } from 'react';
 import { classNames } from '@/shared/libs/classNames/classNames';
 import cls from './Review.module.scss';
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from '@/shared/libs/hooks/useAppDispatch/useAppDispatch';
 import {
-    fetchReviewByIdService,
     getReviewErrorSelector,
     getReviewIsLoadingSelector,
     updateReviewViewCounterService,
@@ -14,7 +13,6 @@ import { ContentSkeleton } from './ContentSkeleton/ContentSkeleton';
 import { Content } from './Content/Content';
 import { VStack } from '@/shared/UI-kit/Stack';
 import { Text } from '@/shared/UI-kit/Text';
-import { useInitialEffect } from '@/shared/libs/hooks/useInitialEffect/useInitialEffect';
 import { useDebounce } from '@/shared/libs/hooks/useDebounce/useDebounce';
 
 interface ReviewProps {
@@ -25,21 +23,26 @@ interface ReviewProps {
 export const Review = memo((props: ReviewProps) => {
     const { className, id } = props;
     const { t } = useTranslation('review');
-    const dispatch = useAppDispatch();
     const isLoading = useSelector(getReviewIsLoadingSelector);
+    const dispatch = useAppDispatch();
     const error = useSelector(getReviewErrorSelector);
 
-    const handleInit = useCallback(() => {
+    const handleUpdateReviewViewCounter = useCallback(() => {
         if (id) {
-            dispatch(fetchReviewByIdService(id));
             dispatch(updateReviewViewCounterService({ reviewId: id }));
         }
     }, [dispatch, id]);
-    const debouncedInit = useDebounce(handleInit, 300);
 
-    useInitialEffect(() => {
-        debouncedInit();
-    });
+    const debounceUpdateReviewViewCounter = useDebounce(
+        handleUpdateReviewViewCounter,
+        300,
+    );
+
+    useEffect(() => {
+        if (id) {
+            debounceUpdateReviewViewCounter();
+        }
+    }, [id]);
 
     let content;
 
