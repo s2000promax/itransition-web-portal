6fc2ef49115a82d0cba26b31cb5811f0cf222a5d
s2000promax <s2000promax@gmail.com>
2023-09-26 22:29:41 +0400

    fix: update review

--- client/src/entities/Review/model/services/updateReview/updateReview.service.ts
+++ client/src/entities/Review/model/services/updateReview/updateReview.service.ts
@@ -1,16 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from '@/app/providers/StoreProvider';
 import { FormDataI } from '../../types/review.interface';
+import { getReviewFormSelector } from '@/entities/Review';
 
 export const updateReviewService = createAsyncThunk<
     void,
     FormDataI,
     ThunkConfig<string>
 >('review/update', async (reviewFormData, thunkApi) => {
-    const { extra, rejectWithValue } = thunkApi;
+    const { extra, getState, rejectWithValue } = thunkApi;
+
+    const formData = getReviewFormSelector(getState());
 
     try {
-        const response = await extra.api.put('review/update', reviewFormData);
+        const response = await extra.api.put('review/update', formData);
 
         if (response.status !== 200) {
             return rejectWithValue('error');
