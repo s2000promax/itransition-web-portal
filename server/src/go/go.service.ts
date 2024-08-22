e9b0182de98a12c0e99906a9226894e84dd5eb2b
Stan Venger <stanislav.v@mygoods.io>
2024-08-22 22:41:22 +0400

    feat: only add go module

--- server/src/go/go.service.ts
+++ server/src/go/go.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class GoService {
     private tempData: any;
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor() {}
 
     saveData(data: any) {
         this.tempData = data;
